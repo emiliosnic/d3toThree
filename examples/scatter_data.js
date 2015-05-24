62ebd8cd0be122371da89f4c73d9ee25c55cfd6b
emiliosnic <emilios_nic@hotmail.com>
2015-05-24 16:20:40 +0100

    Added scatter_data script for scatter graph

---
+++ examples/scatter_data.js
@@ -0,0 +1,11 @@
+var nodes = [
+	{'Name': 'Node 1', 'Category': 'A', 'Calories':10 , 'Protein (g)':4, 'radius': 8  },
+	{'Name': 'Node 2', 'Category': 'A', 'Calories':20 , 'Protein (g)':6, 'radius': 9  },
+	{'Name': 'Node 3', 'Category': 'B', 'Calories':30 , 'Protein (g)':2, 'radius': 8  },
+	{'Name': 'Node 4', 'Category': 'B', 'Calories':40 , 'Protein (g)':2, 'radius': 16  },
+	{'Name': 'Node 5', 'Category': 'A', 'Calories':60 , 'Protein (g)':3, 'radius': 11 },
+	{'Name': 'Node 6', 'Category': 'A', 'Calories':80 , 'Protein (g)':8, 'radius': 10  },
+	{'Name': 'Node 7', 'Category': 'C', 'Calories':63 , 'Protein (g)':7, 'radius': 20 },
+	{'Name': 'Node 8', 'Category': 'C', 'Calories':69 , 'Protein (g)':10, 'radius': 12 },
+	{'Name': 'Node 9', 'Category': 'A', 'Calories':30 , 'Protein (g)':8, 'radius': 12 }
+];
